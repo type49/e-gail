17cc341722db5f3c98a267c29b3d688a12105ee0
type49 <underalmaty@gmail.com>
2021-05-02 01:53:08 +0600

    Добавлено окно настроек и возможность выбора альтернативной темы.

--- main.py
+++ main.py
@@ -12,12 +12,72 @@ import text_processing as tp
 
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtCore import Qt, QEventLoop, QTimer
+from PyQt5.QtCore import Qt, QEventLoop, QTimer, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QWidget, QApplication, QSystemTrayIcon, QAction, QMenu
 from queenui import Ui_MainWindow
+from settingsui import Ui_SettingsWindow
 
 config = configparser.ConfigParser()
 config.read(r'data\settings.ini')
+
+
+class SettingsWin(QtWidgets.QMainWindow, QWidget):
+
+    def __init__(self):
+        with open(r'data\settings.ini', 'r') as config_file:
+            self.old_config = config_file.read()
+        QtWidgets.QWidget.__init__(self, parent=None)
+
+        self.ui = Ui_SettingsWindow()
+        self.ui.setupUi(self)
+        self.setWindowOpacity(0.8)
+        self.show()
+        self.ui.saveButton.clicked.connect(self.save_button)
+        self.ui.cancelButton.clicked.connect(self.cancel_button)
+
+        self.ui.themeComboBox.setCurrentText(config.get("Theme", "background"))
+        self.ui.themeComboBox.activated.connect(self.set_theme_cb)
+        self.ui.themeComboBox.activated.connect(self.set_theme_cb)
+        self._old_pos = None
+
+    on_theme_changed = pyqtSignal(str)
+
+    def set_theme_cb(self):
+        background_image = config.get("Theme", "background")
+        self.on_theme_changed.emit(background_image)
+        a = self.ui.themeComboBox.currentIndex()
+        if a == 0:
+            config.set("Theme", "background", "blue")
+            with open(r'data\settings.ini', "w") as config_file:
+                config.write(config_file)
+        elif a == 1:
+            config.set("Theme", "background", "pink")
+            with open(r'data\settings.ini', "w") as config_file:
+                config.write(config_file)
+
+    def save_button(self):
+        with open(r'data\settings.ini', "w") as config_file:
+            config.write(config_file)
+        self.close()
+
+    def cancel_button(self):
+        with open(r'data\settings.ini', 'w') as config_file:
+            config_file.write(self.old_config)
+        self.close()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self._old_pos = event.pos()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self._old_pos = None
+
+    def mouseMoveEvent(self, event):
+        if not self._old_pos:
+            return
+        delta = event.pos() - self._old_pos
+        self.move(self.pos() + delta)
 
 
 class MyWin(QtWidgets.QMainWindow, QWidget):
@@ -97,6 +157,8 @@ class MyWin(QtWidgets.QMainWindow, QWidget):
         self.tray_icon.show()
         tray_exit_action = QAction("Выход", self)
         tray_exit_action.triggered.connect(QtWidgets.QApplication.quit)
+        tray_settings_action = QAction("Настройки", self)
+        tray_settings_action.triggered.connect(self.settings_foo)
 
         tray_menu = QMenu()
         tray_menu.setStyleSheet('''
@@ -105,6 +167,7 @@ class MyWin(QtWidgets.QMainWindow, QWidget):
                             color: #efe2cd;
                             border: 1px solid #f09ea3;
                                 ''')
+        tray_menu.addAction(tray_settings_action)
         tray_menu.addAction(tray_exit_action)
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
@@ -122,6 +185,58 @@ class MyWin(QtWidgets.QMainWindow, QWidget):
             return
         delta = event.pos() - self._old_pos
         self.move(self.pos() + delta)
+
+    def settings_foo(self):
+        self.sw = SettingsWin()
+        self.sw.show()
+        self.sw.on_theme_changed.connect(self.set_theme_foo)
+
+
+    @pyqtSlot(str)
+    def set_theme_foo(self, backgroundImage):
+        """
+        Change theme from settings.
+        """
+        if backgroundImage == 'blue':
+            self.ui.background.setStyleSheet("image: url(data/images/blue.png);")
+            self.ui.textBrowser.setStyleSheet('''
+                                                            font: 10pt "Montserrat Alternates";
+                                                            background-color: #414547;
+                                                            color: #efe2cd;
+                                                            padding: 5;
+                                                            border: 1px solid #3f81a1;
+                                                            border-radius: 10px;
+                                                                ''')
+            self.ui.queenBrowser.setStyleSheet('''
+                                                            font: 10pt "Montserrat Alternates";
+                                                            background-color: #414547;
+                                                            color: #efe2cd;
+                                                            padding: 5;
+                                                            border: 1px solid #3f81a1;
+                                                            border-radius: 10px;
+                                                                ''')
+            self.ui.textBrowser.setGeometry(100, 390, 310, 33)
+            self.ui.queenBrowser.setGeometry(160, 320, 250, 60)
+        if backgroundImage == 'pink':
+            self.ui.background.setStyleSheet("image: url(data/images/pink.png);")
+            self.ui.textBrowser.setStyleSheet('''
+                                                font: 10pt "Montserrat Alternates";
+                                                background-color: #414547;
+                                                color: #efe2cd;
+                                                padding: 5;
+                                                border: 1px solid #f09ea3;
+                                                border-radius: 10px;
+                                                    ''')
+            self.ui.queenBrowser.setStyleSheet('''
+                                                font: 10pt "Montserrat Alternates";
+                                                background-color: #414547;
+                                                color: #efe2cd;
+                                                padding: 5;
+                                                border: 1px solid #f09ea3;
+                                                border-radius: 10px;
+                                                    ''')
+            self.ui.textBrowser.setGeometry(10, 390, 410, 33)
+            self.ui.queenBrowser.setGeometry(160, 300, 250, 60)
 
 
 class InactiveHotKey(Thread):
